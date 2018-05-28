39a1d170dec973adfda0e3524671d78913991d91
sun98 <1329575336@qq.com>
2018-05-29 00:53:36 +0800

    Fix interface by dalaos' comments

--- data_process/image_preprocessor.py
+++ data_process/image_preprocessor.py
@@ -32,8 +32,28 @@ class ImagePreprocessor(object):
         self.__data_dir = os.path.join(base_dir, 'data')
         self.__width = int(int(width) / 2)
         self.__new_dir_name = new_dir_name
+        self.__labels = {
+            'Cytoplasm': 0,
+            'Mitochondria': 1,
+            'Nucleus but not nucleoli': 2,
+            'Nucleus': 2,
+            'Nucleoli': 2,
+            'Nuclear membrane': 2,
+            'Vesicles': 3,
+            'Endoplasmic reticulum': 4,
+            'Golgi apparatus': 5,
+            'Cytoskeleton (Intermediate filaments)': 6,
+            'Cytoskeleton (Microtubules)': 6,
+            'Cytoskeleton (Actin filaments)': 6,
+            'Microtubule organizing center': 7,
+            'Centrosome': 7,
+            'Plasma membrane': 8,
+            'Focal Adhesions': 9,
+            'Cell Junctions': 9,
+            '': 10
+        }
 
-    def generate_slices_a_set(self, set_id):
+    def __generate_slices_a_set(self, set_id):
         """
         generate patched for a sample data and save them in a new folder
         :param set_id: folder name of an data instance, like 'liver_ENSG00000004975_22914'
@@ -69,7 +89,7 @@ class ImagePreprocessor(object):
                               x + self.__width,
                               y - self.__width:
                               y + self.__width]
-                    new_dir = os.path.join(slice_dir, '{0}_s{1}.jpg'.format(image_name, count))
+                    new_dir = os.path.join(slice_dir, '{0}_p{1}.jpg'.format(image_name, count))
                     cv2.imwrite(new_dir, a_slice)
 
     def generate_patches(self):
@@ -82,7 +102,7 @@ class ImagePreprocessor(object):
         count = 1
         for a_set in sets:
             print('\r', 'slicing {0}/{1}'.format(count, total).ljust(20), end='')
-            self.generate_slices_a_set(a_set)
+            self.__generate_slices_a_set(a_set)
             count += 1
 
     def __get_full_a_set(self, set_id):
@@ -100,8 +120,9 @@ class ImagePreprocessor(object):
                 full_set.append(cv_image)
         return full_set
 
-    def get_dataset_full(self, data_selection='all'):
+    def get_dataset_full(self, data_selection='all', label_type='str'):
         """
+        :param label_type: 'str' or other
         :param data_selection: must be in ['all', 'sup', 'usable']
                 all -> all data
                 sup -> data with supportive labels
@@ -126,27 +147,43 @@ class ImagePreprocessor(object):
 
         # remove <'> at head and tail, while "[]" becomes ""(empty string)
         for i in range(len(pos1)):
-            pos1[i] = pos1[i][1:-1]
-            pos2[i] = pos2[i][1:-1]
-            sets[i] = sets[i].replace('\'', '')
+            pos1[i] = pos1[i].replace('\'', '').replace('[', '').replace(']', '')
+            pos2[i] = pos2[i].replace('\'', '').replace('[', '').replace(']', '')
+            sets[i] = sets[i].replace('\'', '').replace('[', '').replace(']', '')
             reli[i] = reli[i][1:-1]
         if data_selection == 'all':
             for a_set in dir_sets:
                 ensg_index = a_set.split('_')[1]
                 a_full_set = self.__get_full_a_set(a_set)
-                yield pos1[sets.index(ensg_index)], pos2[sets.index(ensg_index)], a_full_set
+                pos_index = sets.index(ensg_index)
+                if label_type == 'str':
+                    yield pos1[pos_index].split(';'), pos2[pos_index].split(';'), a_full_set
+                else:
+                    label1 = [self.__labels[x] for x in pos1[pos_index].split(';')]
+                    label2 = [self.__labels[x] for x in pos2[pos_index].split(';')]
+                    yield label1, label2, a_full_set
         elif data_selection == 'sup':
             suffices = book_sheet.col_values(2)[1:]
             for i in range(len(reli)):
                 if reli[i] == 'Supportive':
                     a_full_set = self.__get_full_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]))
-                    yield pos1[i], pos2[i], a_full_set
+                    if label_type == 'str':
+                        yield pos1[i].split(';'), pos2[i].split(';'), a_full_set
+                    else:
+                        label1 = [self.__labels[x] for x in pos1[i].split(';')]
+                        label2 = [self.__labels[x] for x in pos2[i].split(';')]
+                        yield label1, label2, a_full_set
         elif data_selection == 'usable':
             suffices = book_sheet.col_values(2)[1:]
             for i in range(len(reli)):
                 if reli[i] != '':
                     a_full_set = self.__get_full_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]))
-                    yield pos1[i], pos2[i], a_full_set
+                    if label_type == 'str':
+                        yield pos1[i].split(';'), pos2[i].split(';'), a_full_set
+                    else:
+                        label1 = [self.__labels[x] for x in pos1[i].split(';')]
+                        label2 = [self.__labels[x] for x in pos2[i].split(';')]
+                        yield label1, label2, a_full_set
 
     def __set_width(self, width):
         """
@@ -185,8 +222,27 @@ class ImagePreprocessor(object):
                     patch_set.append(a_patch)
         return patch_set
 
-    def get_dataset_patched(self, size=20, data_selection='all'):
+    def __get_generated_patch_a_set(self, set_id, new_dir_name):
         """
+        :param set_id:
+        :param new_dir_name:
+        :return:
+        """
+        patch_set = []
+        image_dir = os.path.join(self.__data_dir, set_id, new_dir_name)
+        patches = os.listdir(image_dir)
+        os.chdir(image_dir)
+        for a_patch in patches:
+            cv_image = cv2.imread(a_patch)
+            patch_set.append(cv_image)
+        return patch_set
+
+    def get_dataset_patched(self, size=20, data_selection='all', label_type='str', exist=None):
+        """
+        :param exist: None or a string
+                None -> create patches
+                a string -> previously generated patches folder name, e.g. 'new'
+        :param label_type: 'str' or other
         :param size: width of a patch
         :param data_selection: must be in ['all', 'sup', 'usable']
                 all -> all data
@@ -196,6 +252,9 @@ class ImagePreprocessor(object):
                 label string is something like <Nucleus;Cytoplasm;Golgi apparatus> WITHOUT quote
                 cv_data is in the shape of (270*size*size*3)
         """
+        label1s = []
+        label2s = []
+        cv_data = []
         if size is not None:
             self.__set_width(int(int(size) / 2))
         dir_sets = os.listdir(self.__data_dir)
@@ -216,27 +275,75 @@ class ImagePreprocessor(object):
 
         # remove <'> at head and tail, while "[]" becomes ""(empty string)
         for i in range(len(pos1)):
-            pos1[i] = pos1[i][1:-1]
-            pos2[i] = pos2[i][1:-1]
-            sets[i] = sets[i].replace('\'', '')
+            # 这里有个很奇怪的bug，直接去头尾一个字符会导致有的字符串第一个字没了，所以写成这样
+            pos1[i] = pos1[i].replace('\'', '').replace('[', '').replace(']', '')
+            pos2[i] = pos2[i].replace('\'', '').replace('[', '').replace(']', '')
+            sets[i] = sets[i].replace('\'', '').replace('[', '').replace(']', '')
             reli[i] = reli[i][1:-1]
         if data_selection == 'all':
             for a_set in dir_sets:
                 ensg_index = a_set.split('_')[1]
-                a_full_set = self.__get_patch_a_set(a_set)
-                yield pos1[sets.index(ensg_index)], pos2[sets.index(ensg_index)], a_full_set
+                if exist is None:
+                    a_full_set = self.__get_patch_a_set(a_set)
+                else:
+                    a_full_set = self.__get_generated_patch_a_set(a_set, exist)
+                pos_index = sets.index(ensg_index)
+                if label_type == 'str':
+                    # yield pos1[pos_index].split(';'), pos2[pos_index].split(';'), a_full_set
+                    label1s.append(pos1[pos_index].split(';'))
+                    label2s.append(pos2[pos_index].split(';'))
+                    cv_data.append(a_full_set)
+                else:
+                    label1 = [self.__labels[x] for x in pos1[pos_index].split(';')]
+                    label2 = [self.__labels[x] for x in pos2[pos_index].split(';')]
+                    # yield label1, label2, a_full_set
+                    label1s.append(label1)
+                    label2s.append(label2)
+                    cv_data.append(a_full_set)
         elif data_selection == 'sup':
             suffices = book_sheet.col_values(2)[1:]
             for i in range(len(reli)):
                 if reli[i] == 'Supportive':
-                    a_full_set = self.__get_patch_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]))
-                    yield pos1[i], pos2[i], a_full_set
+                    if exist is None:
+                        a_full_set = self.__get_patch_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]))
+                    else:
+                        a_full_set = self.__get_generated_patch_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]),
+                                                                      exist)
+                    if label_type == 'str':
+                        # yield pos1[i].split(';'), pos2[i].split(';'), a_full_set
+                        label1s.append(pos1[i].split(';'))
+                        label2s.append(pos2[i].split(';'))
+                        cv_data.append(a_full_set)
+                    else:
+                        label1 = [self.__labels[x] for x in pos1[i].split(';')]
+                        label2 = [self.__labels[x] for x in pos2[i].split(';')]
+                        # yield label1, label2, a_full_set
+                        label1s.append(label1)
+                        label2s.append(label2)
+                        cv_data.append(a_full_set)
         elif data_selection == 'usable':
             suffices = book_sheet.col_values(2)[1:]
             for i in range(len(reli)):
                 if reli[i] != '':
-                    a_full_set = self.__get_patch_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]))
-                    yield pos1[i], pos2[i], a_full_set
+                    if exist is None:
+                        a_full_set = self.__get_patch_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]))
+                    else:
+                        a_full_set = self.__get_generated_patch_a_set('liver_{}_{}'.format(sets[i], suffices[i][1:-1]),
+                                                                      exist)
+                    if label_type == 'str':
+                        # yield pos1[i].split(';'), pos2[i].split(';'), a_full_set
+                        label1s.append(pos1[i].split(';'))
+                        label2s.append(pos2[i].split(';'))
+                        cv_data.append(a_full_set)
+                    else:
+                        label1 = [self.__labels[x] for x in pos1[i].split(';')]
+                        label2 = [self.__labels[x] for x in pos2[i].split(';')]
+                        # yield label1, label2, a_full_set
+                        label1s.append(label1)
+                        label2s.append(label2)
+                        cv_data.append(a_full_set)
+
+        return label1s, label2s, cv_data
 
 
 if __name__ == '__main__':
@@ -253,7 +360,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # sample usage
-    image_pre = ImagePreprocessor(base_dir=args.base_dir)
-    for l1, l2, d in image_pre.get_dataset_patched(size=20, data_selection='all'):
-        # print(l1, l2, len(d), numpy.array(d).shape)
-        pass
+    # image_pre = ImagePreprocessor(base_dir=args.base_dir)
+    image_pre = ImagePreprocessor(base_dir='D:\\All_Projects\\ML_project\\HPA_ieee')
+    # image_pre.generate_patches()
+    # for l1, l2, d in image_pre.get_dataset_patched(size=20, data_selection='all', label_type='int', exist='new'):
+    #     print(l1, l2, len(d), numpy.array(d).shape)
+    l1, l2, d = image_pre.get_dataset_patched(size=20, data_selection='all', label_type='int', exist='new')
+    for i in range(len(l1)):
+        print(len(l1[i]), len(l2[i]), numpy.array(d[i]).shape)
