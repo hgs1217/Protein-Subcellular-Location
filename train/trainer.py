bfb8ff2801d95eeb260e64fa6e64538824ca7d8c
sun98 <1329575336@qq.com>
2018-06-04 15:17:42 +0800

    redundant commit

--- train/trainer.py
+++ train/trainer.py
@@ -4,12 +4,18 @@
 from train.cnn import CNN
 from data_process.data_preprocessor import generate_patch, data_construction
 
+import os
 
 TARGET_LABELS = [0, 1, 2, 3, 4, 5]
 TEST_RATIO = 8 / 9
 
 
-def train(simple=False, start_step=0, epoch_size=100, keep_pb=0.5, learning_rate=0.001):
+def train(simple=False, start_step=0, gpu=True,epoch_size=100, keep_pb=0.5, learning_rate=0.001):
+    if gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    else:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # use cpu only
+
     raws, labels, test_raws, test_labels, loss_array = data_construction(TARGET_LABELS, TEST_RATIO)
 
     print(raws[0].shape)
