30d3b13659f40b034e466a849612b0aa84be7aa9
gigaflw <a_gigantic_flower@yeah.net>
2018-06-17 23:05:23 +0800

    util path class

--- train_zb/config.py
+++ train_zb/config.py
@@ -2,14 +2,27 @@
 # @Author: gigaflw
 # @Date:   2018-06-01 16:25:21
 # @Last Modified by:   gigaflw
-# @Last Modified time: 2018-06-17 22:53:28
+# @Last Modified time: 2018-06-17 23:05:08
 
 import os
 
+class Path:
+    def __init__(self, path):
+        self.path = path
+
+    def __truediv__(self, path):
+        return Path(os.path.join(self.path, str(path)))
+
+    def __rtruediv__(self, path):
+        return Path(os.path.join(str(path), self.path))
+
+    def __repr__(self):
+        return repr(self.path)
+
 class config:
     model_dir = None
-    base_dataset_dir = os.path.join(os.path.dirname(__file__), '..', 'data_process', 'HPA_ieee')
-    dataset_dir = os.path.join(os.path.dirname(__file__), 'dataset')
+    base_dataset_dir = Path(os.path.dirname(__file__))/'..'/'data_process'/'HPA_ieee'
+    dataset_dir = Path(os.path.dirname(__file__))/'dataset'
 
     # dataset config
     stride = 128
@@ -20,4 +33,6 @@ class config:
     # training config
     shuffle_size = 10
     learning_rate = 0.1
+
+
 
